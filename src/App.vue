7bf1e7d89f6423c8c2386113df459e47ae671e52
Sandro <luiss.morar@gmail.com>
2020-04-21 00:26:57 -0400

    carrito de compras hasta mostrando los productos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="row">
+      <div class="col-md-7">
+        <div class="row">
+          <div class="col-md-6" v-for="prod in productos" :key="prod.id">   <!-- recorrido de arreglo de objetos productos -->
+            <producto :producto="prod"></producto>      <!-- manda objeto información del producto al componente hijo Producto.vue-->
+          </div>
+        </div>
+
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import productos from './productos.json'          //importación de achivo .json
+import Producto from './components/Producto.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Producto    
+  },
+  data() {
+    return {
+      productos                                   // datos archivo productos.json
+    }
+  },
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Producto.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card my-5">
+        <div class="card-body">
+            <h5 class="text-center card-title">{{producto.titulo}}</h5>
+            <img :src="producto.imagen" width="290" height="150">       <!-- se hace un binding para mandar información a la propiedad src -->
+            <p class="text-center text-muted card-text display-4">$ {{ Number(producto.precio).toFixed()}}</p>
+            <button class="btn btn-primary form-control">Agregar al carrito</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Producto',
+    props: ['producto']             // en props recibe datos como un arreglo 
+}
+</script>
+
+<style>
+</style>
